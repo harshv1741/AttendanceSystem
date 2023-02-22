f21c73f53d26dbf2362459d7dda371b0a0489862
harshv1741 <harshv1741@gmail.com>
2023-02-22 12:10:02 +0530

    fix: testing export functionality

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const downloadPDF = () => {
-	var doc = new jspdf.jsPDF("p", "pt", "a4");
+const button = document.querySelector("#export");
 
-	doc.html(document.querySelector("#labortable"), {
-		callback: function (doc) {
-			doc.save("MLB World Series Winners.pdf");
-		},
-		margin: [60, 60, 60, 60],
-		x: 32,
-		y: 32,
-	});
-};
+function downloadPDFWithBrowserPrint() {
+	window.print();
+}
 
-document.querySelector("#export").addEventListener("click", downloadPDF());
+button.addEventListener("click", downloadPDFWithBrowserPrint());
